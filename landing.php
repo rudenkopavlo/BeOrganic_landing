0798ee2d335672f57aa980343f927fa991e21a5c
Pavlo Rudenko <rudenko.pavlo@gmail.com>
2016-08-16 17:02:04 +0200

    remove footer

--- landing.php
+++ landing.php
@@ -1,34 +1,29 @@
+<?php /* Template Name: Example Template */ ?>
 <!DOCTYPE html>
-<html lang="en"><head>
+<html lang="en">
+  <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="блог о здоровой красоте, бьюти блог, блог о здоровом образе жизни, блог органическая косметика, green beauty, киев">
     <meta name="author" content="">
 
-    <title>BeOrganic - сайт о здоровой красоте</title>
+    <title>BeOrganic ♡ о здоровой красоте</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://beorganic.in.ua/wp-content/themes/lifestyle/css/bootstrap.min.css" rel="stylesheet">    
 
     <!-- Custom CSS -->
-    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="http://beorganic.in.ua/wp-content/themes/lifestyle/css/modern-business.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://beorganic.in.ua/wp-content/themes/lifestyle/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
-</head>
+ </head>
 
 <body>
-    
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -70,7 +65,7 @@
         <!-- /.container -->
     </nav>
 
-    <!-- Header Carousel -->
+    <!-- Header Carousel -->    
     <header id="myCarousel" class="carousel slide">
         <!-- Indicators -->
         <ol class="carousel-indicators">
@@ -81,33 +76,35 @@
         </ol>
 
         <!-- Wrapper for slides -->
-        <div class="carousel-inner">
+
+    <div class="carousel-inner">
             <div class="item active">
-                <div class="fill" style="background-image:url('img/nature-flowers-pattern-circle.jpg');"></div>
+                <div class="fill" style="background-image:url('http://beorganic.in.ua/wp-content/themes/lifestyle/img/nature-flowers-pattern-circle.jpg');"></div>
                 <div class="carousel-caption">
                     <h3>eco & organic</h3>
                 </div>
             </div>
             <div class="item">
-                <div class="fill" style="background-image:url('img/food-blueberries-blueberry.jpg');"></div>
+                <div class="fill" style="background-image:url('http://beorganic.in.ua/wp-content/themes/lifestyle/img/food-blueberries-blueberry.jpg');"></div>
                 <div class="carousel-caption">
                     <h3>healthy eating</h3>
                 </div>
             </div>
             <div class="item">
-                <div class="fill" style="background-image:url('img/green_beauty.jpg');"></div>
+                <div class="fill" style="background-image:url('http://beorganic.in.ua/wp-content/themes/lifestyle/img/green_beauty.jpg');"></div>
                 <div class="carousel-caption">
                     <h3>green beauty</h3>
                 </div>
             </div>
             <div class="item">
-                <div class="fill" style="background-image:url('img/mindfulness.jpeg');"></div>
+                <div class="fill" style="background-image:url('http://beorganic.in.ua/wp-content/themes/lifestyle/img/mindfulness.jpeg');"></div>
                 <div class="carousel-caption">
                     <h3>mindfulness</h3>
                 </div>
             </div>
-        </div>
+    </div>
 
+ 
         <!-- Controls -->
         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
             <span class="icon-prev"></span>
@@ -118,7 +115,7 @@
     </header>
 
     <!-- Page Content -->
-    <div class="container" height=auto>
+  <div class="container">
 
         <!-- Marketing Icons Section -->
         <div class="row">
@@ -160,15 +157,15 @@
                     </div>
                 </div>
             </div>
-        </div>
+       </div>      
     </div>
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="http://beorganic.in.ua/wp-content/themes/lifestyle/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="http://beorganic.in.ua/wp-content/themes/lifestyle/js/bootstrap.min.js"></script>
 
     <!-- Script to Activate the Carousel -->
     <script>
@@ -176,6 +173,6 @@
         interval: 2000 //changes the speed
     })
     </script>
-    
 
-</body></html>
+ </body>
+</html>
